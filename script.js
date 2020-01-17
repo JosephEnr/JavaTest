0537b4e0c5c9f794ec929d9815a738ed98605382
JosephEnr <56930038+JosephEnr@users.noreply.github.com>
2020-01-17 13:50:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-for(var i=10;i>0;i--){
+for(var i=50;i>0;i--){
 
-  document.write("For Loop Number: " + i + "<br>")
+  document.write("For Loop #: " + i + "<br>")
 
 }
-var start = 100;
+for(var x=0; x<=30;x++){
+
+  document.write("Random #: " + rng + "<br>")
+
+  var rng = Math.floor(Math.random() * 10)+1;
+
+ }
+var start = 200;
 
 var end = 0;
 
@@ -14,18 +21,11 @@ while(start >= end){
   start=start-10;
 
 }
-for(var x=0; x<=10;x++){
-
-  document.write("Random Number: " + rng + "<br>")
-
-  var rng = Math.floor(Math.random() * 10)+1;
-
- }
-for(var y=0; y<=5;y++){
+for(var y=0; y<=10;y++){
 
   var rand = Math.floor(Math.random() * 10)+1;
 
   var sqrt = Math.sqrt(rand)
 
-  document.write("Random Squared Number: " + sqrt + "<br>")
+  document.write("Random Squared #: " + sqrt + "<br>")
  }
