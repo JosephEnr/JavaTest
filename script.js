d79927014e25014ccb573c4dd198e2b03b5640af
JosephEnr <56930038+JosephEnr@users.noreply.github.com>
2020-01-24 08:49:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,21 @@ for(var y=0; y<=10;y++){
 
   document.write("Random Squared #: " + sqrt + "<br>")
  }
+function myLoop2(){
+  var start2 = document.getElementById("start2").value;
+  var end2 = document.getElementById("end2").value;
+  var jump= document.getElementById("jump").value;
+
+  console.log(typeof start2);
+  console.log(typeof end2);
+  console.log(typeof jump);
+  
+  start2 = Number(start2);
+  end2 = Number(end2);
+  jump = Number(jump);
+  
+    for(var i=start2; i<=end2; i+=jump){
+    document.write('<p>'+ i);
+    
+  }
+}
