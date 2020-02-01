7a0b5006e5b921527c7a7133e1d140d455a60dac
JosephEnr <56930038+JosephEnr@users.noreply.github.com>
2020-01-31 16:37:05 -0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
 function myLoop1(){
 var start = document.getElementById("start").value;
-  
-  var end = document.getElementById("end").value;
+var end = document.getElementById("end").value;
   
   for(var i=start; i<=end; i++){
     document.write('<p>'+i);
